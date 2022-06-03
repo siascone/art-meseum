45fcb8188e031b34e48c398e62fe8fbfcf82e908
siascone <spencer.iascone@gmail.com>
2022-06-03 11:49:52 -0600

    complete art meseum pt 3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryView";
 import {Route} from 'react-router-dom';
+import { Switch } from "react-router-dom";
 
 function App() {
   // console.log(harvardArt);
   return (
     <div className="page-wrapper">
       <GalleryNavigation galleries={harvardArt.records}/>
-      <Route path="/galleries/:galleryId">
-        <GalleryView galleries={harvardArt.records} />
-      </Route>
+
+      <Switch>
+        <Route path="/galleries/:galleryId">
+          <GalleryView galleries={harvardArt.records} />
+        </Route>
+
+        <Route exact path="/">
+          <div>
+            <h2>Harvad Art Meseum</h2>
+            <p>Look, but Don't Touch. Please select a Gallery in the navigation bar.</p>
+          </div>
+        </Route>
+
+        <Route>
+          <div>
+            <h2>Page Not Found</h2>
+          </div>
+        </Route>
+      </Switch>
 
     </div>
   );
